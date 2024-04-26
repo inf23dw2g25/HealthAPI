699ee67b8eb8e6f72562060d4dd5f44fdcf088ae
karim-dev <dr.karim.patata@gmail.com>
2024-04-26 12:10:39 +0200

    Google OAUTH ok

---
+++ Strategies/googleStrategy.js
@@ -0,0 +1,27 @@
+const passport = require('passport');
+const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+const dotenv = require("dotenv");
+const path = require("path");
+
+
+dotenv.config({
+    path: path.resolve(__dirname, "../.env")
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URI,
+    passReqToCallback   : true
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+  }
+));
+
+passport.serializeUser(function(user,done){
+  done(null,user);
+})
+passport.deserializeUser(function(user,done){
+  done(null,user);
+})
--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 const express = require('express');
-const dotenv = require("dotenv");
-const path = require("path");
+const passport = require('passport');
+const session = require('express-session');
+const middleware = require('./controllers/middlewareController')
 
-dotenv.config({
-    path: path.resolve(__dirname, ".env")
-});
+require('./controllers/environmentController')
+require('./Strategies/googleStrategy');
 
 const app = express();
+app.use(session({ secret: process.env.SESSION_SECRET}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req,res) =>{
     res.send('<a href="/auth/google"> Entre com o Google')
 });
 
-app.get('/protected', (req,res)  =>{
-    res.send('Rota Protegida , Parabéns estás Authenticado');
+app.get('/auth/google', 
+    passport.authenticate('google',{scope: ['email','profile']})
+)
+
+app.get('/google/callback',
+    passport.authenticate('google', {
+        successRedirect: '/protected',
+        failureRedirect:'/auth/failure'
+    })
+)
+
+app.get('/auth/failure', (req,res) =>{
+    res.send('Algo deu errado');
 })
+app.get('/protected', middleware,(req ,res)  => {
+    res.send('Rota Protegida , Parabéns estás Authenticado!!!');
+});
+
 app.listen(process.env.NODE_PORT, ()=> console.log("listening on: " + process.env.NODE_PORT))
