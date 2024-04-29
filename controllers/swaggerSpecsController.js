848bc36b1814a21ac705f0b96b7c054c4f7384f5
karim-dev <dr.karim.patata@gmail.com>
2024-04-29 19:01:48 +0200

    Oauth funcional

--- controllers/swaggerSpecsController.js
+++ controllers/swaggerSpecsController.js
@@ -1,49 +1,47 @@
 const swaggerJSDoc = require("swagger-jsdoc");
-require('./environmentController');
+require("./environmentController");
 // swaggerSpecs.js
 
 const swaggerDefinition = {
-    openapi: "3.0.0",
-    info: {
-      title: "HealthApi",
-      version: "1.0.0",
-      description: "Consultas e Históricos",
-      contact: { name: "Grupo 25" },
+  openapi: "3.0.0",
+  info: {
+    title: "HealthApi",
+    version: "1.0.0",
+    description: "Consultas e Históricos",
+    contact: { name: "Grupo 25" },
+  },
+  servers: [
+    {
+      url: `${process.env.PROTOCOL}://${process.env.HOSTNAME}:${process.env.NODE_PORT}`,
+      description: `${process.env.ENV} mode Server.`,
     },
-    servers: [
-      {
-        url: `${process.env.PROTOCOL}://${process.env.HOSTNAME}:${process.env.NODE_PORT}`,
-        description: `${process.env.ENV} mode Server.`,
-      },
-    ],
-    components: {
-      securitySchemes: {
-        OAuth2: {
-          type: "oauth2",
-          flows: {
-            implicit: {
-              authorizationCode: {
-                authorizationUrl: "http://localhost:3000/auth/google?scope=email",
-                scopes: {
-                  mail: "Aqui tera acesso ao email do utilizador",
-                  profile: "Aqui tera acesso ao perfile do utilizador",
-                  read: "Para ler os recursos ",
-                  write: "Para escrever nos recursos"
-                },
-              },
+  ],
+  components: {
+    securitySchemes: {
+      google_oauth: {
+        type: "oauth2",
+        description: "Google OAuth",
+        flows: {
+          authorizationCode: {
+            authorizationUrl: "https://accounts.google.com/o/oauth2/v2/auth",
+            tokenUrl: "https://www.googleapis.com/oauth2/v4/token",
+            scopes: {
+              email: "Aqui tera acesso ao email do utilizador",
+              profile: "Aqui tera acesso ao perfile do utilizador",
             },
           },
         },
       },
     },
-    security: [{ OAuth2: [] }],
-  };
-  
-  const swaggerOptions = {
-    swaggerDefinition,
-    apis: ["./docs/**/*.yaml"], // Substitua pelo caminho correto para seus arquivos YAML
-  };
-  
+  },
+  security: [{ google_oauth: [] }],
+};
+
+const swaggerOptions = {
+  swaggerDefinition,
+  apis: ["./docs/**/*.yaml"], // Substitua pelo caminho correto para seus arquivos YAML
+};
+
 const swaggerSpec = swaggerJSDoc(swaggerOptions);
 
 module.exports = swaggerSpec;
