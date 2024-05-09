0442e4d3a5d0a7a3560cb3ac03a1b8ccf807e4a7
Karim Patatas <kpatatas@MacBook-Pro-de-Karim.local>
2024-05-09 04:01:01 +0200

    POSTMAN COLLECTION ADDED

--- services/perfilService.js
+++ services/perfilService.js
@@ -3,20 +3,26 @@ const middleware = require("../controllers/middlewareController");
 
 async function getPerfil(req, res) {
   try {
-    if (!middleware.isAuthenticatedBool) {
+    // Verifique se o usuário está autenticado chamando o middleware
+    if (!middleware.isAuthenticatedBool(req)) {
       return res.status(401).json({ message: "Usuário não autenticado" });
     }
 
+    // Obtenha o token de acesso do objeto req.user
     const accessToken = req.user.accessToken;
 
-    console.log(google.data);
-    const oauth2Client = new google.oauth2();
+    // Crie o cliente OAuth2 e configure as credenciais de acesso
+    const oauth2Client = new google.auth.OAuth2();
     await oauth2Client.setCredentials({ access_token: accessToken });
 
+    // Crie um cliente OAuth2 para a API do Google
     const oauth2 = google.oauth2({ version: "v2", auth: oauth2Client });
+
+    // Obtenha os detalhes do perfil do usuário
     const { data } = await oauth2.userinfo.get();
     console.log("perfil Service data: " + data);
 
+    // Construa o objeto de perfil do usuário com os dados recebidos
     const perfilUsuario = {
       id: data.id,
       nome: {
@@ -30,6 +36,7 @@ async function getPerfil(req, res) {
       idioma: data.language,
     };
 
+    // Envie o perfil do usuário como resposta
     res.json(perfilUsuario);
   } catch (error) {
     console.error("Erro ao obter o perfil do usuário:", error);
